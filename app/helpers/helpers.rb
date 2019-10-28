e37a1b55be133cc38f201d48565f150b5101cc56
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-28 02:07:32 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -17,14 +17,14 @@
 class Helpers
   
   def self.current_user(session_hash)
-    @user = User.find_by(session_hash[:user_id.to_s])
+    @user = User.find_by(session_hash[:user_id])
     if @user
       @user 
     end 
   end 
   
   def self.is_logged_in?(session_hash)
-    !!(session_hash[:user_id.to_s])
+    !!(session_hash[:user_id])
   end 
   
 end
