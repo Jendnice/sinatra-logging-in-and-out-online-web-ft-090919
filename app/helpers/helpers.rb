f3481cef01f4351cd6f108990659272a17ff8fe2
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-28 02:07:43 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -17,7 +17,7 @@
 class Helpers
   
   def self.current_user(session_hash)
-    @user = User.find_by(session_hash[:user_id])
+    @user = User.find(session_hash[:user_id])
     if @user
       @user 
     end 
