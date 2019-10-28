50bc1c865560cc009264db60b74a36eae4b1629a
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-28 02:09:21 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,23 +1,7 @@
-# class Helpers
-  
-#   def self.current_user(params)
-#     @user = User.find_by(params[:user_id.to_s])
-#     if @user
-#       @user 
-#     end 
-#   end 
-  
-#   def self.is_logged_in?(params)
-#     @user = User.find_by(params[:user_id.to_s])
-#     !!@user 
-#   end 
-  
-# end
-
 class Helpers
   
   def self.current_user(session_hash)
-    @user = User.find(session_hash[:user_id])
+    @user = User.find_by(session_hash[:user_id])
   end 
   
   def self.is_logged_in?(session_hash)
