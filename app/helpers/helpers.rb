3ca4d73d06a86fdd742523ec2c5641e6e7b177ec
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-28 02:05:38 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+  
+  def self.current_user(params)
+    @user = User.find_by(params[:user_id.to_s])
+    if @user
+      @user 
+    end 
+  end 
+  
+  def self.is_logged_in?(params)
+    @user = User.find_by(params[:user_id.to_s])
+    !!@user 
+  end 
+  
 end
+
